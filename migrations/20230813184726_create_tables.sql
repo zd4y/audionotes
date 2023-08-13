39f2529dfaaac7baf16c0c050505bd5956754f36
zd4y <59004538+zd4y@users.noreply.github.com>
2023-08-13 16:09:52 -0500

    fix migration

--- migrations/20230813184726_create_tables.sql
+++ migrations/20230813184726_create_tables.sql
@@ -1,13 +1,16 @@
+create table users (
+        id serial primary key,
+        username varchar (50) unique not null,
+        password varchar (255)
+);
+
 create table audios (
         id serial primary key,
         transcription text,
         file_sha256_hex varchar (64) not null,
-        created_at timestamp not null
-        user_id integer not null
-);
+        created_at timestamp not null,
+        user_id int not null,
 
-create table users (
-        id serial primary key,
-        username varchar (50) unique not null,
-        password varchar (255),
+        foreign key (user_id)
+                references users (id)
 );
