786456691d4b16ee788a914b3ca197303d2969db
zd4y <59004538+zd4y@users.noreply.github.com>
2023-08-27 10:56:47 -0500

    create tables tags and audio_tags

--- migrations/20230813184726_create_tables.sql
+++ migrations/20230813184726_create_tables.sql
@@ -10,8 +10,7 @@ create table audios (
         created_at timestamptz not null default now(),
         user_id int not null,
 
-        foreign key (user_id)
-                references users (id)
+        foreign key (user_id) references users (id)
 );
 
 create table password_reset_tokens (
@@ -20,7 +19,24 @@ create table password_reset_tokens (
         expires_at timestamptz not null default now() + interval '5 minutes',
 
         primary key (user_id, token),
-
-        foreign key (user_id)
-                references users (id)
+        foreign key (user_id) references users (id)
 );
+
+create table tags (
+        user_id int not null,
+        name varchar(25) not null,
+        color varchar(7) not null default '#ffffff',
+
+        primary key (user_id, name),
+        foreign key (user_id) references users (id)
+);
+
+create table audio_tags (
+        tag_user_id int not null,
+        tag_name varchar(25) not null,
+        audio_id int not null,
+
+        primary key (tag_name, audio_id),
+        foreign key (tag_user_id, tag_name) references tags (user_id, name),
+        foreign key (audio_id) references audios (id)
+)
